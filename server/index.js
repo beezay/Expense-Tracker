935c7974355c83b09f3369b2358a91a2f22397ef
beezay <bijay.okur@gmail.com>
2020-07-05 18:33:58 +0545

    final update

--- server/index.js
+++ server/index.js
@@ -12,6 +12,15 @@ const expenses = require('./routes/api/expense');
 
 app.use('/api/expense', expenses);
 
+//Handle Production
+if(process.env.NODE_ENV === 'production'){
+    //static folder
+    app.use(express.static(__dirname + '/public'));
+
+    //Handle Single Page Application
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, ()=> console.log(`App started on port ${port}`));
